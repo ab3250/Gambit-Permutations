b34774a9c89d1e73ac54b0e3b3fd9420986b97e6
AB <johndoe@example.com>
2023-04-18 09:55:00 -0400

    last

--- permutations.scm
+++ permutations.scm
@@ -1,15 +1,27 @@
-#!r7rs
+;#!r7rs
 ;;;;
 (import
   (scheme base)
   (scheme write))
-
+;(install-r7rs!)
 ;1. a  String
 ;2. l  Starting index of the string
 ;3. r Ending index of the string. 
 
-(define (swap a b lst)
-  (define len (length lst))
+(define (mainperm lst)
+  (define retval '())        
+  (define (perm lst stptr endptr)    
+    (if (= stptr endptr)     
+        (set! retval (cons lst retval))
+      (begin                      
+        (let loop ((idx stptr))
+            (if (not (= idx endptr))                  
+              (begin                                                                      
+                (perm (swap stptr idx lst) (+ stptr 1) endptr)                        
+                (loop (+ idx 1))))))))
+
+  (define (swap a b lst)
+    (define len (length lst))
     (let loop ((idx 0)(retlst '()))
       (if (= idx len)           
         retlst
@@ -18,19 +30,9 @@
           ((= idx a) (list-ref lst b))
           ((= idx b) (list-ref lst a))
           ((list-ref lst idx)))))))))
+  
+  (perm lst 0 (length lst))
 
-(define (perm lst stptr endptr)     
-  (if (= stptr endptr)
-    (begin
-      (display lst)(newline))
-    (begin                      
-      (let loop ((idx stptr))
-          (if (not (= idx endptr))                  
-            (begin                                                                      
-              (perm (swap stptr idx lst) (+ stptr 1) endptr)                        
-              (loop (+ idx 1))))))))
+  retval)
 
-(define (main lst)
-  (perm lst 0 (length lst)))
-
-(main '(A B C D))
+(display (mainperm '(A B C D)))
